36a175962ccdbb53238db94aa56120b29a487650
Ness <johnsondavid1107@gmail.com>
2020-11-25 13:54:50 -0500

    created server and conneciton link to DATABASE

--- node.js
+++ node.js
@@ -1,10 +1,2 @@
-const fs = require('fs');
 const inquirer = require('inquirer');
-const express = require("express");
-const mysql = require('mysql');
-
-const app = express();
-
-const PORT = process.env.PORT || 789;
-
-
+const fs = require('fs');
